ef6f5ea09cb83a463c28814c8c153906da00b29f
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-02-22 03:31:57 +0100

    started the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+const { default: Footer } = require("./components/Footer");
+const { default: Header } = require("./components/Header");
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+   
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path = '/' component ={HomeScreen} exact />
+            <Route path = '/product/:id' component ={ProductScreen}  />
+          </Container>
+        </main>
+        <Footer />
+     
+    </Router>
   );
 }
 
